e874c1f156d8623cf7fa44fd3ae7833d35ea92cc
Siddharth Suneel <Siddharth.Suneel@deserve.com>
2024-12-16 20:34:02 +0530

    Initial project structure setup.

---
+++ SiddharthSuneel-Task/Modules/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiddharthSuneel-Task
+//
+//  Created by Siddharth Suneel on 16/12/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
